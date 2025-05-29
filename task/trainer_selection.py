097be354ba36dc5dfdd774776dec2f91b3c95373
YouZhou12138 <2728879990@qq.com>
2025-05-29 17:48:16 +0800

    Add files via upload

---
+++ task/trainer_selection.py
@@ -0,0 +1,12 @@
+from task.st_multi import SpatioTemporal_multi
+from task.st_single import SpatioTemporal_single
+from task.st_variable import SpatioTemporal_variable
+
+Trainer = {
+    "moving_mnist": SpatioTemporal_single,
+    "greenearthnet": SpatioTemporal_multi,
+    "kth": SpatioTemporal_variable,
+    "kth_action": SpatioTemporal_variable,
+    "taxibj": SpatioTemporal_single,
+    "human": SpatioTemporal_single,
+}
